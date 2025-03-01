21e54dce6fa83278e08508cccd6a1f3b55035e57
杨中科 <yangzhongke8@gmail.com>
2025-03-01 23:27:06 +1300

    The first real integration test

---
+++ NetAutoGUI.Windows.IntegrationTests/DynamicMainMenuTests/DynamicMainMenuShould.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using Vanara.PInvoke;
+using Xunit;
+
+namespace NetAutoGUI.Windows.UnitTests.DynamicMainMenuTests;
+
+public class DynamicMainMenuShould
+{
+    [Fact]
+    public void HandleNotePadExit_Correctly()
+    {
+        GUIWindows.Initialize();
+        GUI.Application.KillProcesses("notepad");
+        GUI.Application.LaunchApplication("notepad");
+        GUI.Application.WaitForApplication("notepad");
+        Window? win = GUI.Application.WaitForWindowLikeTitle("*Notepad*");
+        win.Should().NotBeNull();
+        dynamic sut = new DynamicMainMenu(new HWND((IntPtr)win.Id));
+        sut.File.Exit();
+        Thread.Sleep(1000);
+        GUI.Application.IsApplicationRunning("notepad").Should().BeFalse();
+    }
+}
