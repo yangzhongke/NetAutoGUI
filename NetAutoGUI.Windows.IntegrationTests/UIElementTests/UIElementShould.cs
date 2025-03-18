d2fd54e4b249949a8294013dc2ef2a4ec006c4e5
杨中科 <yangzhongke8@gmail.com>
2025-03-19 09:33:52 +1300

    check outut of windows and screens

--- NetAutoGUI.Windows.IntegrationTests/UIElementTests/UIElementShould.cs
+++ NetAutoGUI.Windows.IntegrationTests/UIElementTests/UIElementShould.cs
@@ -1,20 +1,42 @@
 ﻿using Xunit;
 using FluentAssertions;
+using System.Diagnostics;
+using Xunit.Abstractions;
+using System.Windows.Forms;
 
 
 namespace NetAutoGUI.Windows.UnitTests.UIElementTests;
 
 public class UIElementShould
 {
+    private readonly ITestOutputHelper output;
+
+    public UIElementShould(ITestOutputHelper output)
+    {
+        this.output = output;
+    }
+
     [Fact]
-    public void Handle_MsPaint_Correctly()
+    public void Handle_WinFormsAppForTest1_Correctly()
     {
         GUIWindows.Initialize();
-        GUI.Application.LaunchApplication(@".\AppsForTest\WinFormsAppForTest1.exe");
+        Process process = GUI.Application.LaunchApplication(@".\AppsForTest\WinFormsAppForTest1.exe");
+        output.WriteLine("All Screens:");
+        foreach (var screen in Screen.AllScreens)
+        {
+            output.WriteLine($"DeviceName: {screen.DeviceName}, Bounds: {screen.Bounds}");
+        }
+        Thread.Sleep(1000);
+        output.WriteLine("All windows:");
+        foreach (var w in GUI.Application.GetAllWindows())
+        {
+            output.WriteLine(w.Title);
+        }
         Window? win = GUI.Application.WaitForWindowByTitle("WinFormsAppForTest1");
         UIElement? uiWindow = win?.GetRoot();
         uiWindow.ClassName.Should().Contain("WindowsForms");
         uiWindow.Text.Should().Be("WinFormsAppForTest1");
+        uiWindow.Parent.Should().BeNull();
         GUI.Application.KillProcesses("WinFormsAppForTest1");
     }
 }
