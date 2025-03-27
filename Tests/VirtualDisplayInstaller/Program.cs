650b621af72bbc42034d56f6608aaa777d511ff2
杨中科 <yangzhongke8@gmail.com>
2025-03-27 16:22:14 +1300

    Auto install VirtualDisplayInstaller

--- Tests/VirtualDisplayInstaller/Program.cs
+++ Tests/VirtualDisplayInstaller/Program.cs
@@ -5,7 +5,7 @@ GUI.Application.KillProcesses("Virtual.Display.Driver-v24.12.24-setup-x64");
 GUI.Application.KillProcesses("Virtual.Display.Driver-v24.12.24-setup-x64.tmp");
 GUI.Application.LaunchApplication("Virtual.Display.Driver-v24.12.24-setup-x64.exe");
 var winStep1 =
-    GUI.Application.WaitForWindowLikeTitle("Setup*Virtual Display Driver*", 20);
+    GUI.Application.WaitForWindowLikeTitle("Setup*Virtual Display Driver*", 60);
 winStep1.Activate();
 winStep1.WaitForText("I accept the agreement").Click();
 winStep1.WaitForText("Next").Click();
@@ -18,4 +18,8 @@ winStep1.WaitForText("Next").Click();
 winStep1.WaitForText("Next").Click();
 winStep1.WaitForText("Next").Click();
 winStep1.WaitForText("Next").Click();
-winStep1.WaitForText("Install").Click();
+winStep1.WaitForText("Install").MouseClick();
+var confirmDialog =
+    GUI.Application.WaitForWindowLikeTitle("*Windows*Security*", 180);
+confirmDialog.WaitForText("Install").MouseClick();
+winStep1.WaitForText("Finish", 180).Click();
